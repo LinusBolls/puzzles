96d58ddb2c109f1edd91eb815624c965e710b634
Berkand Metehan <Berkand.bm@gmail.com>
2021-08-25 18:03:56 +0200

    baguette hopefully loads faster now

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
+baguetteBox.run('.gallery');
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-})
-
-baguetteBox.run('.gallery');
+});
