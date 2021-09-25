f74edd6029fb68929f59afd35f677966f1235bb1
LinusBolls <82451500+LinusBolls@users.noreply.github.com>
2021-09-25 15:27:15 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+let isAtTop = true;
+const scrollListener = () => {
+  const scrollTop =
+    window.pageYOffset ||
+    (document.documentElement || document.body.parentNode || document.body)
+      .scrollTop;
+  if (scrollTop > 0 && isAtTop) {
+    isAtTop = false;
+    document.querySelector(".header").classList.add("header--solid");
+  }
+  if (scrollTop <= 0 && !isAtTop) {
+    isAtTop = true;
+    document.querySelector(".header").classList.remove("header--solid");
+  }
+};
+const toggleFlyout = () => {
+  const flyout = document.querySelector(".flyoutMenu");
+  const flyoutShadow = document.querySelector(".flyoutShadow");
+  const button = document.querySelector(".menuButton");
+  flyout.classList.toggle("flyoutMenu--open");
+  flyout.classList.toggle("flyoutMenu--closed");
+  button.classList.toggle("menuButton--open");
+  flyoutShadow.classList.toggle("flyoutShadow--open");
+};
+window.addEventListener("load", () => {
+  document.querySelector(".header__menuButton").onclick = toggleFlyout;
+  document.querySelector(".flyoutShadow").onclick = toggleFlyout;
+  document.onscroll = scrollListener;
+
+  const galleryColumns = [
+    [
+      ["img_10.jpg", "Bar"],
+      ["img_27.jpg", "Nether"],
+      ["img_30.jpg", "Rosenzimmer"],
+      ["img_29.jpg", "Glory Hole"],
+    ],
+    [
+      ["img_19.jpg", "Blue Light Bereich"],
+      ["img_26.jpg", "Pilzzimmer"],
+      ["img_20.jpg", "Blue Light Bereich"],
+    ],
+    [
+      ["img_17.jpg", "Massagestuhl"],
+      ["img_15.jpg", "Umkleideraum"],
+      ["img_5.jpg", "Wohnzimmer"],
+      ["img_18.jpg", "Blue Light Bereich"],
+    ],
+  ];
+  document.querySelector(".gallery").innerHTML = galleryColumns
+    .map(
+      column =>
+        "<div class='gallery__column'>" +
+        column
+          .map(
+            ([path, desc]) =>
+              `<a href="gallery/img/${path}" data-caption="${desc}">
+  <img src="gallery/img/${path}" alt="${desc}">
+</a>`
+          )
+          .join("") +
+        "</div>"
+    )
+    .join("");
+  baguetteBox.run(".gallery");
+});
--- script/script.js
+++
@@ -1,8 +0,0 @@
-baguetteBox.run('.gallery');
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-});
